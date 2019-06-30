08fe6b9509974e2474d87d19695a9a4b0243ce68
lucaperrig <52051084+lucaperrig@users.noreply.github.com>
2019-06-30 15:29:16 +0200

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## This code provides an efficient way to compute the inverse of a matrix.
+
+## The following function is a cache function that will store the
+## computed inverted matrices in order to retreive the solutions later.
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+## The following function computes the inverse of a matrix, but more efficiently
+## because it firts tries to retrieve the solution if already computed.
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
